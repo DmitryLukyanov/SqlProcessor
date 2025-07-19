34f42f26c3db24c2543f98b7d5e5d51a0c0318b9
DmitryLukyanov <dzima.lukyanau@gmail.com>
2025-07-20 02:24:56 +0400

    Minor renamings

---
+++ Analyzers/AnalyzersHelper/DiagnosticIds.cs
@@ -0,0 +1,8 @@
+﻿namespace AnalyzersHelper
+{
+    // TODO: use this helper in analyzers
+    public class DiagnosticIds
+    {
+        public const string UnresolvedReference = "TRSP003";
+    }
+}
--- Analyzers/AnalyzersHelper/Diagnostics.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AnalyzersHelper
-{
-    // TODO: use this helper
-    public class Diagnostics
-    {
-        public const string UnresolvedReferenceDiagntosticCode = "TRSP003";
-    }
-}
